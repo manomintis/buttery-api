fa1f0b542c38a0004ae986c72f4402575e8d9d57
Boris Abramov <{ID}+{username}@users.noreply.github.com>
2026-08-15 05:59:14 -0400

    Install SQLite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrganizationsModule } from './organizations/organizations.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: 'db.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    OrganizationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
